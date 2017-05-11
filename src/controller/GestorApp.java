894a71272da4b2b5fe76a9034e70334292c4c259
Juaki97 <garcia.jjimenez@gmail.com>
2017-05-11 23:55:00 +0200

    - Añadida e implementada opción para leer clientes (empresa) e insertarlos en el gestor.

--- src/controller/GestorApp.java
+++ src/controller/GestorApp.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Cliente;
+import model.Empresa;
 import model.Gestor;
 import model.Persona;
 
@@ -26,8 +27,7 @@ public class GestorApp {
                     gestor.registarCliente(leerPersona());
                     break;
                 case 2:
-                    if (gestor.longitud()>0){
-                    }
+                    gestor.registarCliente(leerEmpresa());
                     break;
                 case 3:
                     if (gestor.longitud()>0){
@@ -46,6 +46,7 @@ public class GestorApp {
 
         System.out.println("**********************************");
         System.out.println("* 1. Registrar cliente (persona) *");
+        System.out.println("* 2. Registrar cliente (empresa) *");
         if (gestor.longitud()>0) {
             System.out.println("* 3. Clientes registrados        *");
         }
@@ -108,6 +109,60 @@ public class GestorApp {
         fechaAlta = new Date();
 
         cliente = new Persona(direccionFacturacion, telfContacto, fechaAlta, nombre, apellidos, email, telf, dni);
+
+        return cliente;
+    }
+
+    public Cliente leerEmpresa(){
+        Scanner input = new Scanner (System.in);
+        String nombre;
+        String nif;
+        String direccion;
+        String encargado;
+        int telfEncargado;
+        String direccionFacturacion;
+        int telfContacto;
+        Date fechaAlta;
+        Cliente cliente;
+
+        do {
+            System.out.println("Nombre Empresa: ");
+            nombre = input.next();
+        }while (nombre.equals(""));
+
+        do {
+            System.out.println("NIF Empresa: ");
+            nif = input.next();
+        }while (nif.equals(""));
+
+        do {
+            System.out.println("Direccion Empresa: ");
+            direccion = input.next();
+        }while (direccion.equals(""));
+
+        do {
+            System.out.println("Nombre Encargado Empresa: ");
+            encargado = input.next();
+        }while (encargado.equals(""));
+
+        do {
+            System.out.println("Télefono Encargado: ");
+            telfEncargado = input.nextInt();
+        }while (telfEncargado <= 0);
+
+        do {
+            System.out.println("Dirección de Facturación: ");
+            direccionFacturacion = input.next();
+        }while (direccionFacturacion.equals(""));
+
+        do {
+            System.out.println("Télefono Contacto: ");
+            telfContacto = input.nextInt();
+        }while (telfContacto <= 0);
+
+        fechaAlta = new Date();
+
+        cliente = new Empresa(direccionFacturacion, telfContacto, fechaAlta, nombre, nif, direccion, encargado, telfEncargado);
 
         return cliente;
     }

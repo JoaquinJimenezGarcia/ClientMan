cc2cecf6441bfe98520365ee02c694fe43638492
Juaki97 <garcia.jjimenez@gmail.com>
2017-05-16 02:20:01 +0200

    - Añadidas clases "ClientesPendientes" y "ClientesRecibidos" que gestionará LinkedList con los clientes que han pedido una thermomix y los que la han recibido ya respectivamente

---
+++ src/model/ClientesPendientes.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Created by garci on 16/05/2017.
+ */
+public class ClientesPendientes {
+    private LinkedList<Cliente> clientesPendientes;
+
+    public ClientesPendientes(){
+        clientesPendientes = new LinkedList<>();
+    }
+
+    public void registarCliente(Cliente cliente){
+        if (clientesPendientes.contains(cliente)){
+            cliente = null;
+            System.out.println("Ya hay un cliente con este NIF/DNI");
+        }
+
+        if (cliente != null){
+            clientesPendientes.add(cliente);
+        }
+    }
+
+    public int longitud(){
+        return clientesPendientes.size();
+    }
+
+    public void mostrarClientes(){
+        Collections.sort(clientesPendientes, Cliente.comparadorPorID);
+
+        for (Cliente cliente: clientesPendientes){
+            System.out.println(cliente);
+        }
+    }
+
+    public void borrarCliente(int id){
+        Iterator<Cliente> itCliente = clientesPendientes.iterator();
+        while (itCliente.hasNext()){
+            Cliente cliente = itCliente.next();
+
+            if (cliente.getId() == id){
+                itCliente.remove();
+            }
+        }
+    }
+}
---
+++ src/model/ClientesRecibidos.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Created by garci on 16/05/2017.
+ */
+public class ClientesRecibidos {
+    private LinkedList<Cliente> clientesRecibidos;
+
+    public ClientesRecibidos(){
+        clientesRecibidos = new LinkedList<>();
+    }
+
+    public void registarCliente(Cliente cliente){
+        if (clientesRecibidos.contains(cliente)){
+            cliente = null;
+            System.out.println("Ya hay un cliente con este NIF/DNI");
+        }
+
+        if (cliente != null){
+            clientesRecibidos.add(cliente);
+        }
+    }
+
+    public int longitud(){
+        return clientesRecibidos.size();
+    }
+
+    public void mostrarClientes(){
+        Collections.sort(clientesRecibidos, Cliente.comparadorPorID);
+
+        for (Cliente cliente: clientesRecibidos){
+            System.out.println(cliente);
+        }
+    }
+
+    public void borrarCliente(int id){
+        Iterator<Cliente> itCliente = clientesRecibidos.iterator();
+        while (itCliente.hasNext()){
+            Cliente cliente = itCliente.next();
+
+            if (cliente.getId() == id){
+                itCliente.remove();
+            }
+        }
+    }
+}

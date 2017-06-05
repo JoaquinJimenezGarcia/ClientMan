5c12ba3da0938e6e8844ea7dc32a0ae187d22742
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-06-05 17:23:59 +0200

    - Modificada bienvenida

--- src/controller/GestorApp.java
+++ src/controller/GestorApp.java
@@ -52,7 +52,7 @@ public class GestorApp {
         try {
             usuarioFinal = listaUsuarios.contiene(usuario);
             if (usuarioFinal != null) {
-                System.out.println("Bienvenido " + usuarioFinal);
+                System.out.println("Bienvenido " + usuarioFinal.getNombre() + "!!");
                 run();
             }
         } catch (NullPointerException e) {
